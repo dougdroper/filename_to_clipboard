bc551d2b636b86f5909cbeccf1300c51a7281de3
Douglas Roper <douglasroper@notonthehighstreet.com>
2013-03-07 10:15:13 +0000

    grab the timestamp of a migration

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 In sublime text 2 when running ruby spec's
-I want to copy the path of the spec to the clipboard.
+I want to copy the relative path of the spec to the clipboard.
 Otherwise I just want to copy the whole path.
+When in db/migrate I only want the timestamp of the file.
 
 (On mac) Move the filename_to_clipboard_command.py into
 ~/Library/Application Support/Sublime Text 2/Packages/User
--- filename_to_clipboard_command.py
+++ filename_to_clipboard_command.py
@@ -1,10 +1,17 @@
 #Add this to the User Keybindings
 #{ "keys": ["ctrl+super+t"], "command": "filename_to_clipboard" },
 
-import sublime, sublime_plugin, os
+import sublime, sublime_plugin, os, re
 
 class FilenameToClipboardCommand(sublime_plugin.TextCommand):
-   def run(self, edit):
+    def spec(self, s):
+      return s if s.find("spec/") == -1 else s[s.find('spec'):]
+
+    def migrate(self, s):
+      return s if s.find("migrate/") == -1 else s.split("/")[-1].split("_")[0]
+
+    def run(self, edit):
       s = str(self.view.file_name())
-      s = s if s.find("spec") == -1 else s[s.find('spec'):]
+      s = self.migrate(s)
+      s = self.spec(s)
       sublime.set_clipboard(s)
